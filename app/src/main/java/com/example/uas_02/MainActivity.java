27b43a0e54be748e7547b3a34647e8c2bf9294dc
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-06-21 18:06:14 +0700

    soal 2

---
+++ app/src/main/java/com/example/uas_02/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.uas_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.animation.ObjectAnimator;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView mImgView;
+    Bitmap mBitmap;
+    Canvas mCanvas;
+    private int mColorBackground;
+    Paint mCirclePaint = new Paint();
+    Paint mHeadPaint = new Paint();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImgView = findViewById(R.id.my_img_view);
+
+        mCirclePaint.setColor(getResources().getColor(R.color.black));
+        mHeadPaint.setColor(getResources().getColor(R.color.white));
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        int vWidth = mImgView.getWidth();
+        int vHeight = mImgView.getHeight();
+        float centerX = vWidth / 2f;
+        float centerY = vHeight / 2f;
+        float radiusX = vWidth / 3f;
+        float radiusY = vHeight / 4f;
+
+        mBitmap = Bitmap.createBitmap(vWidth, vHeight, Bitmap.Config.ARGB_8888);
+        mImgView.setImageBitmap(mBitmap);
+        mColorBackground = ResourcesCompat.getColor(getResources(), R.color.yellow, null);
+        mCanvas = new Canvas(mBitmap);
+        mCanvas.drawColor(mColorBackground);
+
+        drawHead(centerX, centerY, radiusX, radiusY);
+        drawRightEye(vWidth, vHeight);
+        drawLeftEye(vWidth, vHeight);
+        drawEyeConnector(vWidth, vHeight);
+    }
+
+    public void drawHead(float centerX, float centerY, float radiusX, float radiusY) {
+        mCanvas.drawOval(centerX - radiusY, centerY - radiusX, centerX + radiusY,
+                centerY + radiusX, mHeadPaint);
+    }
+
+    public void drawRightEye (int mWidth, int mHeight) {
+        mCanvas.drawCircle(mWidth / 2 - 200,mHeight / 2, 80, mCirclePaint);
+    }
+
+    public void drawLeftEye (int mWidth, int mHeight) {
+        mCanvas.drawCircle(mWidth / 2 + 200,mHeight / 2, 80, mCirclePaint);
+    }
+
+    public void drawEyeConnector (int mWidth, int mHeight) {
+        mCanvas.drawRect(mWidth / 2 - 175, mHeight / 2 + 20,
+                mWidth / 2 + 175, mHeight / 2 -20, mCirclePaint);
+    }
+
+
+}
